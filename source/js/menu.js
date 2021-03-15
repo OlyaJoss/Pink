4f1ee2ccee7ff0379e94344da18921b35031cf53
OlyaJoss <65188310+OlyaJoss@users.noreply.github.com>
2021-03-15 18:21:59 +0300

    правки по критериям часть 1

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,19 +1,19 @@
-let navigationButton = document.querySelector('.page-header__toggle');
-let navigationMenu = document.querySelector('.main-nav');
-let navigationBackground = document.querySelector('.page-header__wrapper');
+let navigationButton = document.querySelector(".page-header__toggle");
+let navigationMenu = document.querySelector(".main-nav");
+let navigationBackground = document.querySelector(".page-header__wrapper");
 
 
 let clickButtonHandler = function () {
 
-  navigationButton.classList.toggle('page-header__toggle--opened');
-  navigationButton.classList.toggle('page-header__toggle--closed');
+  navigationButton.classList.toggle("page-header__toggle--opened");
+  navigationButton.classList.toggle("page-header__toggle--closed");
 
-  navigationMenu.classList.toggle('main-nav--opened');
-  navigationMenu.classList.toggle('main-nav--closed');
+  navigationMenu.classList.toggle("main-nav--opened");
+  navigationMenu.classList.toggle("main-nav--closed");
 
-  navigationBackground.classList.toggle('page-header__wrapper--opened');
-  navigationBackground.classList.toggle('page-header__wrapper--closed');
+  navigationBackground.classList.toggle("page-header__wrapper--opened");
+  navigationBackground.classList.toggle("page-header__wrapper--closed");
 
 };
 
-navigationButton.addEventListener('click', clickButtonHandler);
+navigationButton.addEventListener("click", clickButtonHandler);
